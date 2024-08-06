08d4535bad95a440a5d11a0766d445f4ee811f72
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-08-06 07:37:05 +0700

    aw

--- exam/config/database/mongoose.js
+++ exam/config/database/mongoose.js
@@ -1,37 +1,58 @@
-const mongoose  = require('mongoose')
-const fs        = require('fs')
-const path      = require('path')
-const env       = require('../../env')
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const env = require("../../env");
+const { fileURLToPath } = require("url");
+const { dirname } = require("path");
 
-const clusterUrl = `mongodb://${env.mongoose.username}:${env.mongoose.password}@${env.mongoose.host}:${env.mongoose.port || '27017'}/${env.mongoose.database}?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`
-const ca = `${path.join(__dirname, `../../ssl/${ env.mongoose.cert || 'rds-combined-ca-bundle.pem'}`)}`
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-let mongoConn
+const clusterUrl = `mongodb://${env.mongoose.username}:${
+  env.mongoose.password
+}@${env.mongoose.host}:${env.mongoose.port || "27017"}/${
+  env.mongoose.database
+}?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
+const ca = `${path.join(
+  __dirname,
+  `../../ssl/${env.mongoose.cert || "rds-combined-ca-bundle.pem"}`
+)}`;
 
-if (env.db_type === 'mongodb_aws') {
-    mongoConn = mongoose.connect(`${clusterUrl}`, {
-        tlsCAFile: `${ca}`,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => {
-        return true
-    }).catch((e) => {
-        console.log(e)
-        return false
+let mongoConn;
+
+if (env.db_type === "mongodb_aws") {
+  mongoConn = mongoose
+    .connect(`${clusterUrl}`, {
+      tlsCAFile: `${ca}`,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
+    .then(() => {
+      return true;
+    })
+    .catch((e) => {
+      console.log(e);
+      return false;
+    });
 } else {
-    mongoConn = mongoose.connect(`mongodb://${env.mongoose.host}:${env.mongoose.port}/${env.mongoose.database}`, {
-        auth: { "authSource": "admin" },
+  mongoConn = mongoose
+    .connect(
+      `mongodb://${env.mongoose.host}:${env.mongoose.port}/${env.mongoose.database}`,
+      {
+        auth: { authSource: "admin" },
         user: env.mongoose.username,
         pass: env.mongoose.password,
         useNewUrlParser: true,
-        useUnifiedTopology: true, 
-    }).then(() => {
-        return true
-    }).catch((e) => {
-        console.log(e)
-        return false
+        useUnifiedTopology: true,
+      }
+    )
+    .then(() => {
+      return true;
     })
+    .catch((e) => {
+      console.log(e);
+      return false;
+    });
 }
 
-module.exports = mongoConn
+module.exports = mongoConn;
