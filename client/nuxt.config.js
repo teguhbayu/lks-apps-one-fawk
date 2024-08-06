1300fca86bfaecd8981b00824ec97a7dab62da20
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-08-06 11:04:44 +0700

    sassaa

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -6,29 +6,25 @@ export default {
     titleTemplate: '%s - client',
     title: 'client',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  target: 'static',
+  target: 'server',
   telemetry: false,
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -47,12 +43,20 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    proxy: true
+    proxy: true,
   },
 
   proxy: {
-    '/api/': { target: `${process.env.API_URL}`, pathRewrite: {'^/api/': ''}, changeOrigin: true },
-    '/apiv2/': { target: `${process.env.API_EXAM_URL}`, pathRewrite: {'^/apiv2/': ''}, changeOrigin: true }
+    '/api/': {
+      target: `${process.env.API_URL}`,
+      pathRewrite: { '^/api/': '' },
+      changeOrigin: true,
+    },
+    '/apiv2/': {
+      target: `${process.env.API_EXAM_URL}`,
+      pathRewrite: { '^/apiv2/': '' },
+      changeOrigin: true,
+    },
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
@@ -68,19 +72,18 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+          success: colors.green.accent3,
+        },
+      },
     },
     defaultAssets: {
       font: {
-        family: 'Roboto' 
+        family: 'Roboto',
       },
-      icons: 'md'
+      icons: 'md',
     },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
